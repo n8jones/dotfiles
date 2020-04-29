7e0ec3a549051b1ef4dce495fccda7805e237ca5
Nate Jones <n8jones@gmail.com>
2020-04-29 09:22:07 -0400

    Matching tmux colors to the rest of my terminal

--- bin/status-right.sh
+++ bin/status-right.sh
@@ -4,4 +4,9 @@ date_time() {
   date '+%Y-%m-%d %H:%M'
 }
 
-echo "#[fg=#ff7100]#[fg=default,bg=#ff7100] $(hostname) #[fg=#ce0079,bg=#ff7100]#[fg=default,bg=#ce0079] $(date_time) "
+bg1='blue'
+fg1='black'
+bg2='red'
+fg2='black'
+
+echo "#[fg=$bg1]#[fg=$fg1,bg=$bg1] $(hostname) #[fg=$bg2]#[fg=$fg2,bg=$bg2] $(date_time) "
