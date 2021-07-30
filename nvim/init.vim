8b75649b1d3bd67306b5b7b5c8b681cce2b2a983
Nate Jones <n8jones@gmail.com>
2021-07-30 13:29:48 -0400

    adding native lsp

--- nvim/init.vim
+++ nvim/init.vim
@@ -24,6 +24,10 @@ call plug#begin(stdpath('data') . '/plugged')
   Plug 'nvim-lua/popup.nvim'
   Plug 'nvim-lua/plenary.nvim'
   Plug 'nvim-telescope/telescope.nvim'
+  Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
+  Plug 'neovim/nvim-lspconfig'
+  Plug 'hrsh7th/nvim-compe'
+  Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 call plug#end()
 
 " Settings
@@ -36,33 +40,24 @@ let g:vimwiki_list = g:vimwiki_list + [{'path': '~/Documents/Notes/', 'syntax': 
 let g:rainbow_active = 1
 
 set clipboard+=unnamedplus
+set cmdheight=2 " Give more space for displaying messages.
+set completeopt=menuone,noselect
 set expandtab
+set foldmethod=marker
+set hidden " TextEdit might fail if hidden is not set.
 set list
 set listchars=eol:$,tab:-⇥,trail:~,extends:>,precedes:<
 set mouse=a
-set number
-set relativenumber
-set softtabstop=2
-set shiftwidth=2
-set tabstop=2
-set foldmethod=marker
-set wildignore+=*\\target\\*,*\\.svn\\*,*\\.git\\*,*\\workspace\\*,*\\build\\*
-set hidden " TextEdit might fail if hidden is not set.
 set nobackup " Some servers have issues with backup files, see #649.
 set nowritebackup
-set cmdheight=2 " Give more space for displaying messages.
-" Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
-" delays and poor user experience.
-set updatetime=300
+set number
+set relativenumber
+set shiftwidth=2
 set shortmess+=c " Don't pass messages to |ins-completion-menu|.
-" Always show the signcolumn, otherwise it would shift the text each time
-" diagnostics appear/become resolved.
-if has("patch-8.1.1564")
-  " Recently vim can merge signcolumn and number column into one
-  set signcolumn=number
-else
-  set signcolumn=yes
-endif
+set softtabstop=2
+set tabstop=2
+set updatetime=300
+set wildignore+=*\\target\\*,*\\.svn\\*,*\\.git\\*,*\\workspace\\*,*\\build\\*
 
 " Key mappings
 imap jj <Esc>
@@ -75,12 +70,17 @@ nnoremap <leader>tf <cmd>Telescope find_files<cr>
 nnoremap <leader>tg <cmd>Telescope live_grep<cr>
 nnoremap <leader>tb <cmd>Telescope buffers<cr>
 nnoremap <leader>th <cmd>Telescope help_tags<cr>
+nnoremap <leader>ts <cmd>Telescope lsp_document_symbols<cr>
+nnoremap <leader>tS <cmd>Telescope lsp_dynamic_workspace_symbols<cr>
+
 nnoremap <S-Tab> <<_
 nnoremap <Tab> >>_
 vnoremap <S-Tab> <gv
 vnoremap <Tab> >gv
 
 colorscheme gruvbox
+
+lua require('myluaconfig')
 
 echo 'End of init.vim'
 
---
+++ nvim/lua/myluaconfig.lua
@@ -0,0 +1,129 @@
+local lspconfig = require'lspconfig'
+local system_name
+
+if vim.fn.has("mac") == 1 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+  system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
+  print("Unsupported system")
+end
+
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+  };
+}
+
+local my_on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+end
+
+local sumneko_root_path = vim.fn.expand('$HOME/dev/3rdparty/lua-language-server')
+local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+lspconfig.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+      telemetry = { enable = false },
+    },
+  },
+  on_attach = my_on_attach
+}
+
+lspconfig.jdtls.setup {
+  cmd = {
+    --util.path.join(tostring(vim.fn.getenv 'JAVA_HOME'), '/bin/java'),
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xms1g',
+    '-Xmx2G',
+    '-jar',
+    tostring(vim.fn.getenv 'JDTLS_JAR'),
+    '-configuration',
+    tostring(vim.fn.getenv 'JDTLS_CONFIG'),
+    '-data',
+    tostring(vim.fn.getenv 'JDTLS_WORKSPACE'),
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens java.base/java.util=ALL-UNNAMED',
+    '--add-opens java.base/java.lang=ALL-UNNAMED',
+  },
+  on_attach = my_on_attach
+}
+
+--[[ Groovyls is not ready for primetime, but keeping the config to try out in the future
+lspconfig.groovyls.setup {
+  cmd = {
+    'java',
+    '-jar',
+    tostring(vim.fn.getenv 'GROOVYLS_JAR')
+  },
+  on_attach = my_on_attach
+}
+]]--
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true
+  },
+  indent = {
+    enable = true
+  }
+}
+
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
