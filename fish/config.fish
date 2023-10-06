8627c9435374fd3d05224ed4b712825cae1de83d
Nate Jones <n8jones@gmail.com>
2023-10-06 11:55:14 -0400

    Python path add

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,7 @@ set -x PATH ~/bin/groovy/bin:$PATH
 
 which lsd >> /dev/null; and alias ls=lsd
 
+
+# Setting PATH for Python 3.11
+# The original version is saved in /Users/jonesn7/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
