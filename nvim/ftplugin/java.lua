733acf65d53bf9aa60322fb82c4ff9a191c20fc6
Nate Jones <n8jones@gmail.com>
2023-10-09 16:36:33 -0400

    Pathing out the jdtls path

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,13 +1,13 @@
-local install_path = require('mason-registry').get_package('jdtls'):get_installed_path()
+local jdtls_bin = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
+vim.print("jdtls_bin: " .. jdtls_bin)
 require('jdtls').start_or_attach({
   root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
   cmd = {
-    install_path .. '/bin/jdtls',
+    jdtls_bin,
     '-data',
     tostring(vim.fn.getenv 'JDTLS_WORKSPACE'),
   },
   on_attach = function(client, bufnr)
     require('jdtls.setup').add_commands()
-    --require'myluaconfig'.lsp_on_attach(client, bufnr)
   end,
 })
