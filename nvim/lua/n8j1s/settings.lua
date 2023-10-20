46862a83c5cf13604e5d1d2de2a708cb0e23f499
Nate Jones <n8jones@gmail.com>
2023-10-20 15:51:30 -0400

    Setting up status line.

--- nvim/lua/n8j1s/settings.lua
+++ nvim/lua/n8j1s/settings.lua
@@ -6,3 +6,6 @@ vim.opt.shiftwidth = 2
 vim.opt.listchars = "eol:$,tab:-⇥,trail:~,extends:>,precedes:<"
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.laststatus = 3
+vim.opt.statusline = '%f%M%=[%Y] %{FugitiveStatusline()}'
+
