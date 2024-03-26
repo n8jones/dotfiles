1a3ab3a94d521da64c594e3f3290e00ba37c2f49
Nate Jones <n8jones@gmail.com>
2024-03-26 10:13:54 -0400

    Adding autocmds to calculate times in time logs

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,5 @@ vim.g.mapleader = " "
 require("n8j1s.plugins")
 require("n8j1s.settings")
 require("n8j1s.keymaps")
+require("n8j1s.autocmds")
 
---
+++ nvim/lua/n8j1s/autocmds.lua
@@ -0,0 +1,84 @@
+--[[
+- 08:01 Start
+- 10:30 Second task
+- 12:33 Something else @SCA
+- 13:00 @foo @bar
+- 15:00 /stop
+
+--]]
+local function format_mins(mins)
+  return string.format('%02d:%02d', mins/60, math.fmod(mins, 60))
+end
+local function extract_tags(name)
+  local tags = {}
+  local c = 1
+  while true do
+    local s,e = string.find(name, '@[A-Za-z0-9_]+', c, false)
+    if not s then break end
+    c = e
+    table.insert(tags, string.sub(name, s, e))
+  end
+  return tags
+end
+local function timelog_diagnostics(args)
+  local regex = vim.regex("- \\d\\d:\\d\\d .*")
+  local ns = vim.api.nvim_create_namespace('n8j1s.timelog')
+  vim.diagnostic.reset(ns, args.buf)
+  local diags = {}
+  local add_diag = function (lnum, message)
+    table.insert(diags, {
+      lnum = lnum,
+      col = 0,
+      message = message,
+      severity = 'Info',
+    });
+  end
+  local line_count = vim.api.nvim_buf_line_count(args.buf)
+  local last_task = nil
+  local total_mins = 0
+  local tag_totals = {}
+  local last_line = nil
+  for i = 0, line_count-1, 1
+  do
+    local match = regex:match_line(args.buf, i)
+    if match then
+      last_line = i
+      local line = vim.api.nvim_buf_get_lines(args.buf, i, i+1, true)[1]
+      local hours = tonumber(string.sub(line, 3, 4))
+      local minutes = tonumber(string.sub(line, 6, 7))
+      local name = string.sub(line, 9, -1)
+      local current_task = {
+        name = name,
+        mins = hours * 60 + minutes,
+        lnum = i,
+        tags = extract_tags(name),
+      }
+      if last_task then
+        local diff_min = current_task.mins - last_task.mins
+        total_mins = total_mins + diff_min
+        for _, tag in ipairs(last_task.tags) do
+          local prev = tag_totals[tag]
+          if not prev then prev = 0 end
+          tag_totals[tag] = prev + diff_min
+        end
+        add_diag(last_task.lnum, format_mins(diff_min) .. ' ' .. vim.inspect(last_task.tags))
+      end
+      if string.find(current_task.name, '/', 1) then
+        last_task = nil
+      else
+        last_task = current_task
+      end
+    end
+  end
+  if last_line then
+    local msg = 'Total: ' .. format_mins(total_mins)
+    for tag, mins in pairs(tag_totals) do
+      msg = msg .. string.format(' %s: %s', tag, format_mins(mins))
+    end
+    add_diag(last_line + 1, msg)
+  end
+  vim.diagnostic.set(ns, args.buf, diags)
+end
+
+local grp = vim.api.nvim_create_augroup('n8j1s', { clear = true })
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufEnter' }, { group = grp, callback = timelog_diagnostics, pattern = {'*.md'} })
