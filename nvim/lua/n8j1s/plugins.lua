93d6bee2a4b753bc86f816a23eda5b627113dccf
Nate Jones <n8jones@gmail.com>
2024-04-02 12:46:03 -0400

    Reorganizing dependencies and updating Lazy plugins.

--- nvim/lua/n8j1s/plugins.lua
+++ nvim/lua/n8j1s/plugins.lua
@@ -11,27 +11,35 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {'christoomey/vim-tmux-navigator'},
   {'folke/which-key.nvim', event = "VeryLazy", opts = { } },
-  {'hrsh7th/vim-vsnip', dependencies = { {'hrsh7th/vim-vsnip-integ'}, {'hrsh7th/cmp-vsnip'}, {'rafamadriz/friendly-snippets'}, }, config = delegate('vsnip') },
-  {'hrsh7th/cmp-emoji'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/cmp-buffer'},
-  {'hrsh7th/cmp-path'},
-  {'hrsh7th/cmp-cmdline'},
-  {'hrsh7th/nvim-cmp', event = 'InsertEnter', config = delegate('cmp') },
-  {'LukasPietzschmann/telescope-tabs', dependencies = { 'nvim-telescope/telescope.nvim' }, config = delegate('telescope_tabs') },
-  {'mfussenegger/nvim-jdtls'},
-  {'mickael-menu/zk-nvim', config = delegate('zk') },
+  {'hrsh7th/vim-vsnip', event = "InsertEnter", config = delegate('vsnip'), dependencies = {
+    'hrsh7th/vim-vsnip-integ',
+    'rafamadriz/friendly-snippets',
+  } },
+  {'hrsh7th/nvim-cmp', event = 'InsertEnter', config = delegate('cmp'), dependencies = {
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-emoji',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-vsnip',
+  } },
   {'morhetz/gruvbox', config = delegate('gruvbox'), priority = 1000, lazy = false},
-  {'neovim/nvim-lspconfig', cmd = {'LspInfo', 'LspInstall', 'LspStart'}, event = {'BufReadPre', 'BufNewFile'}, dependencies = { {'hrsh7th/cmp-nvim-lsp'}, {'williamboman/mason-lspconfig.nvim'}, }, config = delegate('lspconfig') },
-  {'nvim-telescope/telescope.nvim', dependencies = { 'nvim-telescope/telescope-fzf-native.nvim', 'nvim-lua/plenary.nvim', } },
-  {'nvim-treesitter/nvim-treesitter', config = delegate('treesitter'), version='*' },
-  {'sindrets/diffview.nvim'},
+  {'neovim/nvim-lspconfig', cmd = {'LspInfo', 'LspInstall', 'LspStart'}, event = {'BufReadPre', 'BufNewFile'}, config = delegate('lspconfig'), dependencies = {
+    {'mfussenegger/nvim-jdtls'},
+    {'williamboman/mason-lspconfig.nvim', version = '*'},
+  } },
+  {'nvim-telescope/telescope.nvim', dependencies = {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    'nvim-lua/plenary.nvim',
+  } },
+  {'nvim-treesitter/nvim-treesitter', config = delegate('treesitter'), version='*', dependencies = {
+    'nvim-treesitter/nvim-treesitter-context',
+  } },
   {'tpope/vim-fugitive'},
   {'tpope/vim-surround'},
   {'voldikss/vim-floaterm', config = delegate('floaterm')},
   {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x', lazy = true, config = false, init = delegate('lsp_zero'), },
-  {'williamboman/mason.nvim', lazy = false, config = true},
+  {'williamboman/mason.nvim', lazy = false, config = true, version = '*'},
 })
 
