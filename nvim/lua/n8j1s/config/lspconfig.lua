904e4a596bdfb229e966c808fd77625889c25a7b
Nate Jones <n8jones@gmail.com>
2024-04-02 12:46:35 -0400

    Adding lua lsp support.

--- nvim/lua/n8j1s/config/lspconfig.lua
+++ nvim/lua/n8j1s/config/lspconfig.lua
@@ -1,9 +1,12 @@
 local M = {}
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 function M.config()
   local lsp_zero = require('lsp-zero')
   lsp_zero.extend_lspconfig()
 
-  lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.on_attach(function(_, bufnr)
     lsp_zero.default_keymaps({buffer = bufnr})
     local km = require('n8j1s.km')
     km.n('<leader>ca', vim.lsp.buf.code_action)
@@ -12,11 +15,38 @@ function M.config()
   end)
 
   require('mason-lspconfig').setup({
-    ensure_installed = {'jdtls'},
+    ensure_installed = {
+      'jdtls',
+      'lua_ls',
+    },
     handlers = {
       lsp_zero.default_setup,
       jdtls = lsp_zero.noop, -- delegate to java.lua
+      lua_ls = M.lua_ls_setup,
     }
   })
 end
+
+function M.lua_ls_setup()
+  require('lspconfig').lua_ls.setup({
+    capabilities = lsp_capabilities,
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT'
+        },
+        diagnostics = {
+          globals = {'vim'},
+        },
+        workspace = {
+          library = {
+            vim.env.VIMRUNTIME,
+            vim.opt.rtp,
+          }
+        }
+      }
+    }
+  })
+end
+
 return M
