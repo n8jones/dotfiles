33121b7960eff5074a7366b28d8b7803886b761b
Nate Jones <n8jones@gmail.com>
2024-04-03 09:06:44 -0400

    Moving jdt config into the mason-lspconfig handlers.

--- nvim/lua/n8j1s/config/lspconfig.lua
+++ nvim/lua/n8j1s/config/lspconfig.lua
@@ -9,9 +9,9 @@ function M.config()
   lsp_zero.on_attach(function(_, bufnr)
     lsp_zero.default_keymaps({buffer = bufnr})
     local km = require('n8j1s.km')
-    km.n('<leader>ca', vim.lsp.buf.code_action)
-    km.n('gd', vim.lsp.buf.definition)
-    km.n('K', vim.lsp.buf.hover);
+    --km.n('<leader>ca', vim.lsp.buf.code_action)
+    --km.n('gd', vim.lsp.buf.definition)
+    --km.n('K', vim.lsp.buf.hover);
   end)
 
   require('mason-lspconfig').setup({
@@ -21,7 +21,7 @@ function M.config()
     },
     handlers = {
       lsp_zero.default_setup,
-      jdtls = lsp_zero.noop, -- delegate to java.lua
+      jdtls = M.jdtls_setup,
       lua_ls = M.lua_ls_setup,
     }
   })
@@ -46,6 +46,48 @@ function M.lua_ls_setup()
         }
       }
     }
+  })
+end
+
+function M.jdtls_setup()
+  local mason_pkg = require('mason-registry').get_package('jdtls')
+  if not mason_pkg:is_installed() then
+    print 'jdtls mason package not installed'
+    return
+  end
+  local jdtls_bin = mason_pkg:get_install_path() .. '/jdtls'
+  if vim.fn.has('win64') then
+    jdtls_bin = jdtls_bin .. '.cmd'
+  end
+  local rootDir = vim.fs.dirname(vim.fs.find({'settings.gradle', '.git'}, { upward = true })[1])
+  if not rootDir then
+    rootDir = vim.fn.getcwd()
+  end
+  local wsDir = vim.fn.getenv('JDTLS_WORKSPACE')
+  if (wsDir == vim.NIL or string.len(wsDir) == 0) then
+    wsDir = rootDir .. '/../.workspace/' .. vim.fs.basename(rootDir) .. '.workspace'
+  end
+  require('jdtls').start_or_attach({
+    root_dir = rootDir,
+    cmd = {
+      jdtls_bin,
+      '-data',
+      wsDir,
+    },
+    settings = {
+      java = {
+        signatureHelp = { enabled = true },
+        completion = {
+          favoriteStaticMembers = {},
+          filteredTypes = {
+            "com.sun.*",
+            "java.awt.*",
+            "jdk.*",
+            "sun.*",
+          },
+        },
+      }
+    },
   })
 end
 
