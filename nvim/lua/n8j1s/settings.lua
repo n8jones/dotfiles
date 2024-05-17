7c9a3b80cb7fd815055d9435393d22e7d2608626
Nate Jones <n8jones@gmail.com>
2024-05-17 10:25:03 -0400

    Removing old statusline setting.

--- nvim/lua/n8j1s/settings.lua
+++ nvim/lua/n8j1s/settings.lua
@@ -7,11 +7,12 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '~', nbsp = '␣', eol = '$'}
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.statusline = '%f%M%=[%Y] %{FugitiveStatusline()}'
 vim.opt.smartcase = true
 vim.opt.signcolumn = 'yes'
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.wrap = true
 vim.opt.breakindentopt = 'list:-1'
+
+vim.g.markdown_fenced_languages = {'ps1'}
 
