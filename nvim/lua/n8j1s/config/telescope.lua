41f7cd5dbdb680a8dff285254eec72f2c10e00dc
Nate Jones <n8jones@gmail.com>
2024-07-15 09:36:32 -0400

    Increasing the size of the telescope window.

--- nvim/lua/n8j1s/config/telescope.lua
+++ nvim/lua/n8j1s/config/telescope.lua
@@ -2,6 +2,19 @@ local M = {}
 
 function M.config()
   require("telescope").setup {
+    defaults = {
+      layout_strategy = "flex",
+      layout_config = {
+        horizontal = {
+          width = 0.97,
+          height = 0.9,
+        },
+        vertical = {
+          width = 0.97,
+          height = 0.9,
+        },
+      },
+    },
     extensions = {
       ["ui-select"] = {
         require("telescope.themes").get_dropdown {
